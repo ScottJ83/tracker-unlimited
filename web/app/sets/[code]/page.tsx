86cef9c111e9513e940bee4c46456ad85efe5d6d
ScottJ83 <83jscott83@gmail.com>
2026-04-03 23:48:22 -0400

    force dynamic set page

--- web/app/sets/[code]/page.tsx
+++ web/app/sets/[code]/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { redirect } from "next/navigation";
 import ProgressBar from "@/components/ProgressBar";
 import SetClient from "@/components/SetClient";
