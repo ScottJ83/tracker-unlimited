06e15ebebe4fd7fd774ec8b9480dc3896ec8ae74
ScottJ83 <83jscott83@gmail.com>
2026-04-03 20:10:21 -0400

    fix card actions props

--- web/components/CardActions.tsx
+++ web/components/CardActions.tsx
@@ -1,33 +1,14 @@
 "use client";
 
-import { useState } from "react";
 import AddCardButton from "./AddCardButton";
 
 export default function CardActions({
   userId,
   cardId,
-  variants,
 }: any) {
-  const [variantId, setVariantId] = useState(variants?.[0]?.id);
-
   return (
     <div style={{ marginTop: "8px" }}>
-      <select
-        value={variantId}
-        onChange={(e) => setVariantId(e.target.value)}
-      >
-        {variants?.map((v: any) => (
-          <option key={v.id} value={v.id}>
-            {v.name}
-          </option>
-        ))}
-      </select>
-
-      <AddCardButton
-        userId={userId}
-        cardId={cardId}
-        variantId={variantId}
-      />
+      <AddCardButton userId={userId} cardId={cardId} />
     </div>
   );
 }
